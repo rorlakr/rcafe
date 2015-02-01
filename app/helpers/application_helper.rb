673b3307d1a84b1a73cd62611eb0f26ee3066a75
Lucius Choi <rorlab@gmail.com>
2015-02-01 17:03:57 +0900

    flash 메시지 표시하기

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when "success"
+        "alert-success"   # 초록색
+      when "error"
+        "alert-danger"    # 빨간색
+      when "alert"
+        "alert-warning"   # 노랑색
+      when "notice"
+        "alert-info"      # 파랑색
+      else
+        flash_type.to_s
+    end
+  end
 end
