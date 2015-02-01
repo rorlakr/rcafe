5598120c8117fd1484841bc7325f9291fbf4189c
Lucius Choi <rorlab@gmail.com>
2015-02-01 20:04:14 +0900

    기본 데이터 추가하기

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# 디폴트 게시판 생성
+Bulletin.create! title: '공지사항'
+Bulletin.create! title: '새소식'
+Bulletin.create! title: '가입인사', post_type: 'blog'
+Bulletin.create! title: '갤러리', post_type: 'gallery'
